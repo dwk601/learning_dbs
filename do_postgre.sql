b6d141b0eb8e6732496d43b2818a57744d7f8aa0
dwk601 <10829539@uvu.edu>
2025-01-16 15:17:56 -0700

    Enhance PostgreSQL script with additional queries and index creation for contacts table

--- do_postgre.sql
+++ do_postgre.sql
@@ -106,3 +106,74 @@ SELECT pg_size_pretty(pg_relation_size('contacts')) AS table_size;
 VACUUM ANALYZE contacts;
 
 -- question 6
+SELECT indexname FROM pg_indexes WHERE tablename = 'contacts' AND indexdef LIKE '%contact_id%';
+
+EXPLAIN ANALYZE SELECT * FROM contacts WHERE contact_id = 10081;
+
+CREATE INDEX idx_contact_id ON contacts(contact_id);
+
+EXPLAIN SELECT * FROM contacts WHERE contact_id = 10081;
+
+-- question 7
+SELECT reltuples AS estimated_rows
+FROM pg_class
+WHERE relname = 'contacts';
+
+SELECT n_live_tup AS estimated_rows
+FROM pg_stat_all_tables
+WHERE relname = 'contacts';
+
+-- question 8
+ALTER TABLE contacts SET (autovacuum_enabled = off);
+
+DELETE FROM contacts WHERE contact_id IN (SELECT contact_id FROM contacts ORDER BY contact_id LIMIT 30000);
+
+ANALYZE contacts;
+
+CREATE EXTENSION pgstattuple;
+
+SELECT * FROM pgstattuple('contacts');
+
+VACUUM contacts;
+
+SELECT * FROM pgstattuple('contacts');
+
+SELECT pg_relation_size('contacts') AS table_size;
+
+VACUUM FULL contacts;
+
+ANALYZE contacts;
+
+SELECT * FROM pgstattuple('contacts');
+
+SELECT pg_relation_size('contacts') AS table_size;
+
+ALTER TABLE contacts SET (autovacuum_enabled = on);
+
+-- question 9
+CREATE INDEX idx_areacode ON contacts (areacode);
+
+SELECT pg_relation_size('idx_areacode') AS index_size_bytes;
+
+EXPLAIN SELECT * FROM contacts WHERE areacode = '801';
+
+CREATE INDEX idx_areacode_ut ON contacts (areacode) WHERE areacode IN ('801', '435', '385');
+
+SELECT pg_relation_size('idx_areacode_ut') AS partial_index_size_bytes;
+
+EXPLAIN SELECT * FROM contacts WHERE areacode = '801';
+
+ANALYZE contacts;
+
+-- question 10
+SELECT pg_relation_size('contacts') AS table_size_before;
+
+ALTER TABLE contacts ADD COLUMN mydata TEXT;
+
+SELECT pg_relation_size('contacts') AS table_size_after_add;
+
+UPDATE contacts SET mydata = 'Fear is the mind killer.';
+
+SELECT pg_relation_size('contacts') AS table_size_after_update;
+
+SELECT contact_id, mydata FROM contacts WHERE contact_id = 1;
